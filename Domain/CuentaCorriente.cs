e61a7213bbc61e755a2e1b93b76a1210e6a9d73b
RodriGB0510 <Rodrigo.OrdonezNadra@alu.frt.utn.edu.ar>
2025-05-01 20:27:32 -0300

    Se refactorizo con herencia, se utilizaron propiedades, y se sobreescribieron los metodos

---
+++ Domain/CuentaCorriente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain;
+
+public class CuentaCorriente : CuentaBancaria
+{
+    public CuentaCorriente(string Numero, decimal Saldo, Estado Estado, string[] Titulares) : base(Numero, Saldo, Estado, Titulares)
+    {
+    }
+    public decimal LimiteDeDescubierto { get; init; }
+    public decimal Comision { get; init; }
+    public override void Depositar(decimal monto)
+    {
+        monto -= monto * Comision;
+        Saldo += monto;
+    }
+    public override void Retirar(decimal monto)
+    {
+        if (monto <= 0)
+            throw new MontoNoValidoException();
+        if(!Estado.Activa)
+            throw new CuentaNoActivaException();
+        if (Saldo - monto < -LimiteDeDescubierto)
+        {
+            throw new SaldoInsuficienteException();
+            Estado = Estado.Suspendida;
+        }
+        Saldo -= monto;
+    }
+}
