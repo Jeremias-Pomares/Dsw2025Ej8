7462fa110933e1927bac45534bdfd34a9e7daf67
lilbbxd <anycarobaena@gmail.com>
2025-05-02 18:29:37 -0300

    "nada"

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Dsw2025Ej8
+﻿using Dsw2025Ej8.Domain;
+
+namespace Dsw2025Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            PruebaHandler.InstanciarCuentas();
+            PruebaHandler.OperarConCuentas(Caso.C5_Retirar_FALLO);
+            PruebaHandler.RecorrerCuentas();
         }
         
     }
